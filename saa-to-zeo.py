0ee6de1d33e1c8afff0ca5b06bbce7107869dca0
lucas <mail@limitlesschannels.com>
2013-02-22 17:46:08 -0800

    moved reformatting into separate function

--- saa-to-zeo.py
+++ saa-to-zeo.py
@@ -37,6 +37,26 @@ def retitle(title):
 	else:
 		return title
 
+def reorder(unordered):
+	order = []
+	order.append(unordered[4])
+	order.append(unordered[2])
+	order.append(unordered[3])
+	order.append(unordered[5])
+	order.extend(unordered[9:])
+	return order
+
+def reformat(unformatted):
+	import string
+	formatlist = []
+	formatlist.extend(unformatted)
+	formatlist[3] = hours2min(unformatted[3])
+	for i in range(3):
+		formatlist[i] = retitle(unformatted[i])
+	formatlist[3] = retitle(formatlist[3])
+	return ",".join(formatlist)
+
+
 import sys
 
 SAAINFILE = sys.argv[1]
@@ -49,23 +69,13 @@ outlist = []
 
 while instr:
 	orderedline = []
-	formatline = []
 	linelist = instr.split(",")
-	del linelist[0:2]
-	del linelist[4:7]
-	orderedline.append(linelist.pop(2))
-	orderedline.extend(linelist[:])
-	formatline.extend(orderedline)
-	formatline[3] = hours2min(orderedline[3])
-	for i in range(3):
-		formatline[i] = retitle(orderedline[i])
-	formatline[3] = retitle(formatline[3])
-	newlinestr = ",".join(formatline)
+	orderedline = reorder(linelist)
+	newlinestr = reformat(orderedline)
 	outlist.append(newlinestr)
 	instr = infile.readline()
 
 outstr = "\n".join(outlist)
-
 outfile.write(outstr)
 infile.close()
 outfile.close()
